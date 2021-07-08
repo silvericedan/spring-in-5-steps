600d90b908645b4d03e0edf478faa88be20cfecc
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2021-07-08 17:40:47 -0300

    added quicksort as @Component, and added @Primary to be choosen first when spring tries to find and create the bean

--- src/main/java/com/silvericedan/spring/basics/springin5steps/services/QuickSortAlgorithm.java
+++ src/main/java/com/silvericedan/spring/basics/springin5steps/services/QuickSortAlgorithm.java
@@ -1,5 +1,10 @@
 package com.silvericedan.spring.basics.springin5steps.services;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Primary
 public class QuickSortAlgorithm implements SortAlgorithm {
 
     public int[] sort(int[] numbers){
