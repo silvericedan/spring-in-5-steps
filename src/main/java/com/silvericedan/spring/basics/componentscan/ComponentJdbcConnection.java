bf1f16e08821caaab20595a9468fb001d09b8bb7
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2021-07-13 11:01:34 -0300

    lesson 23 created simple ComponentDAO class

---
+++ src/main/java/com/silvericedan/spring/basics/componentscan/ComponentJdbcConnection.java
@@ -0,0 +1,19 @@
+package com.silvericedan.spring.basics.componentscan;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+
+//When we want to have a class make a new prototype, we can use ConfigurableBeanFactory.SCOPE_PROTOTYPE
+//But in this case, because JdbcConnection is a dependency of PersonDAO, then it doesnt make a new instance.
+//To solve that, we use proxyMode to make the new instances of JdbcConnection
+@Component
+@Scope(value=ConfigurableBeanFactory.SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.TARGET_CLASS)
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class ComponentJdbcConnection {
+    public ComponentJdbcConnection() {
+        System.out.println("JDBC CONNECTION");
+    }
+}
---
+++ src/main/java/com/silvericedan/spring/basics/springin5steps/SpringIn5StepsComponentScanApplication.java
@@ -0,0 +1,26 @@
+package com.silvericedan.spring.basics.springin5steps;
+
+import com.silvericedan.spring.basics.componentscan.ComponentDAO;
+import com.silvericedan.spring.basics.springin5steps.scope.PersonDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringIn5StepsComponentScanApplication {
+
+	//We use this Logger to print in console when the app runs with "INFO ....."
+	private static Logger LOGGER = LoggerFactory.getLogger(SpringIn5StepsComponentScanApplication.class);
+
+	public static void main(String[] args) {
+
+
+		ApplicationContext applicationContext = SpringApplication.run(SpringIn5StepsComponentScanApplication.class, args);
+		ComponentDAO componentDao = applicationContext.getBean(ComponentDAO.class);
+
+		LOGGER.info("{}", componentDao);
+	}
+
+}
