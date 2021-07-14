8b67a1303c45e3cacc5840c59f1fe98ea8cde381
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2021-07-14 15:52:50 -0300

    defining application context with beans loaded trought component scan in xml configuration

--- src/main/java/com/silvericedan/spring/basics/springin5steps/SpringIn5StepsXMLContextApplication.java
+++ src/main/java/com/silvericedan/spring/basics/springin5steps/SpringIn5StepsXMLContextApplication.java
@@ -1,25 +1,25 @@
 package com.silvericedan.spring.basics.springin5steps;
 
-import com.silvericedan.spring.basics.springin5steps.basic.BinarySearchImpl;
 import com.silvericedan.spring.basics.springin5steps.xmlscope.XmlPersonDAO;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-@Configuration
-@ComponentScan
 public class SpringIn5StepsXMLContextApplication {
 
-
+	//We use this Logger to print in console when the app runs with "INFO ....."
+	private static Logger LOGGER = LoggerFactory.getLogger(SpringIn5StepsComponentScanApplication.class);
 
 	public static void main(String[] args) {
-
 
 		try(ClassPathXmlApplicationContext applicationContext =
 					new ClassPathXmlApplicationContext("applicationContext.xml")){
 
 			XmlPersonDAO personDAO = applicationContext.getBean(XmlPersonDAO.class);
+
+			//We want to see what are the beans that are currently in this app
+			//We cast to (Object) because otherwise it only prints the first bean loaded, the first element of the array
+			LOGGER.info("Beans Loaded -> {}", (Object)applicationContext.getBeanDefinitionNames());
 
 			System.out.println(personDAO);
 			System.out.println(personDAO.getXmlJdbcConnection());
