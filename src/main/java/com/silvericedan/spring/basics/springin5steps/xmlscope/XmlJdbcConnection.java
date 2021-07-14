8579668a3adfc2cd6c45038067f1dc3d71a8d581
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2021-07-14 10:09:48 -0300

    created a simple xml applicationcontext xml, an alternative to config beans instead of using annotations

---
+++ src/main/java/com/silvericedan/spring/basics/springin5steps/xmlscope/XmlJdbcConnection.java
@@ -0,0 +1,7 @@
+package com.silvericedan.spring.basics.springin5steps.xmlscope;
+
+public class XmlJdbcConnection {
+    public XmlJdbcConnection() {
+        System.out.println("XMLJDBC CONNECTION");
+    }
+}
---
+++ src/main/java/com/silvericedan/spring/basics/springin5steps/xmlscope/XmlPersonDAO.java
@@ -0,0 +1,15 @@
+package com.silvericedan.spring.basics.springin5steps.xmlscope;
+
+
+public class XmlPersonDAO {
+
+    XmlJdbcConnection xmlJdbcConnection;
+
+    public XmlJdbcConnection getXmlJdbcConnection() {
+        return xmlJdbcConnection;
+    }
+
+    public void setXmlJdbcConnection(XmlJdbcConnection xmlJdbcConnection) {
+        this.xmlJdbcConnection = xmlJdbcConnection;
+    }
+}
